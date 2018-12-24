d02558af7bf23a19649666247a0b5210925f3f56
Diego Moreira <diegopm07@gmail.com>
2018-12-24 03:58:13 -0200

    Updates

---
+++ precache-manifest.d1cd8782321577d929310b2b2dbf1c33.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = [
+  {
+    "revision": "fee66e712a8a08eef5805a46892932ad",
+    "url": "/GithubCompare/static/media/fontawesome-webfont.fee66e71.woff"
+  },
+  {
+    "revision": "b06871f281fee6b241d60582ae9369b9",
+    "url": "/GithubCompare/static/media/fontawesome-webfont.b06871f2.ttf"
+  },
+  {
+    "revision": "af7ae505a9eed503f8b8e6982036873e",
+    "url": "/GithubCompare/static/media/fontawesome-webfont.af7ae505.woff2"
+  },
+  {
+    "revision": "912ec66d7572ff821749319396470bde",
+    "url": "/GithubCompare/static/media/fontawesome-webfont.912ec66d.svg"
+  },
+  {
+    "revision": "674f50d287a8c48dc19ba404d20fe713",
+    "url": "/GithubCompare/static/media/fontawesome-webfont.674f50d2.eot"
+  },
+  {
+    "revision": "e9d10cfaf911ae1d099b",
+    "url": "/GithubCompare/static/js/runtime~main.e9d10cfa.js"
+  },
+  {
+    "revision": "9a8aa740f7d68e6ee753",
+    "url": "/GithubCompare/static/js/main.9a8aa740.chunk.js"
+  },
+  {
+    "revision": "de15177b412ec7469e0a",
+    "url": "/GithubCompare/static/js/1.de15177b.chunk.js"
+  },
+  {
+    "revision": "de15177b412ec7469e0a",
+    "url": "/GithubCompare/static/css/1.c497ee99.chunk.css"
+  },
+  {
+    "revision": "cc5af965f6db861b48d8981b7b17127b",
+    "url": "/GithubCompare/index.html"
+  }
+];
